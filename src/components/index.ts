a513a5901f6aa82238832a55afabd1967adeff68
maikeg-alves <maicongabrielalves99@gmai.com>
2023-01-10 19:05:52 -0300

    fixbug: error deploy import modal

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@ import FormProject from './formProject/formProject.componet';
 import FormTechnology from './formTechnology/formTechnology.componet';
 import TableItems from './Table/TableItem.component';
 import Table from './Table/Table.component';
-import { useModal, Modal } from './Modal/Modal.component';
+import { useModal, Modal } from './modal/modal.component';
 import GetView from './GetView/GetView.component';
 import Delete from './Delete/Delete.component';
 import ErrorMessage from './ErrorMessage/ErrorMessage.component';
